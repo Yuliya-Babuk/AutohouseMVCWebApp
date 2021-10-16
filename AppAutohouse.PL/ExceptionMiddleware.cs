fb991f83af4e2c73e21445607c788d6313e90333
Yuliya-Babuk <yuliya.babuk@eveoncontainers.com>
2021-10-16 18:22:46 +0300

    1. Add user authentication, role based access 2. Add exception middleware

---
+++ AppAutohouse.PL/ExceptionMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace AppAutohouse.PL
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e?.Message);
+                Log.Error(e?.InnerException?.Message);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
--- AppAutohouse.PL/Startup.cs
+++ AppAutohouse.PL/Startup.cs
@@ -1,17 +1,17 @@
 using AppAutohouse.BLL;
 using AppAutohouse.DAL.Context;
+using AppAutohouse.DAL.Entities;
 using AppAutohouse.PL.Mappers;
 using AutoMapper;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MVCAppAutohouse.DAL.Entities;
-using MVCAppAutohouse.DAL.Interfaces;
 using MVCAppAutohouse.DAL.Repositories;
 using Serilog;
 using System;
@@ -32,7 +32,7 @@ namespace AppAutohouse.PL
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options=>options.Filters.Add(new AuthorizeFilter()));
             services.AddControllersWithViews()
                 .AddViewOptions(options => options.HtmlHelperOptions.ClientValidationEnabled = true);
 
@@ -41,21 +41,25 @@ namespace AppAutohouse.PL
             services.AddDbContext<AutohouseContext>(options =>
             options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Autohouse;Trusted_Connection=True;"));
 
+            //services.AddDbContext<AutohouseContext>(options =>
+            //options.UseInMemoryDatabase("TestDb"));
+
             services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddDefaultUI()
                 .AddEntityFrameworkStores<AutohouseContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddScoped<IRepository<Car>, CarRepository>();
+            services.AddScoped<CarRepository>();
             services.AddScoped<ICarService, CarService>();
-            services.AddScoped<IRepository<Brand>, BrandRepository>();
-            services.AddScoped<IBrandService, BrandService>();
+            services.AddScoped<BrandRepository>();
+            services.AddScoped<IBrandService, BrandService>(); 
+            services.AddScoped<RequestRepository>();
+            services.AddScoped<IRequestService, RequestService>();
+            services.AddScoped<IRoleIdentifierService, RoleIdentifierService>();
 
             Log.Logger = new LoggerConfiguration()
                      .WriteTo.Console()
                         .CreateLogger();
-
-
 
             var mapperConfig = new MapperConfiguration(mc =>
             {
@@ -69,6 +73,8 @@ namespace AppAutohouse.PL
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
+            app.UseExceptionMiddleware();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -92,7 +98,7 @@ namespace AppAutohouse.PL
         private async Task CreateRoles(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var roles = new[] { "admin", "customer", "manager" };
+            var roles = new[] { "admin", "manager" };
 
             foreach (var roleName in roles)
                 await roleManager.CreateAsync(new IdentityRole
@@ -102,13 +108,16 @@ namespace AppAutohouse.PL
                 });
 
             var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var user = await userManager.FindByEmailAsync(Configuration["AdminUserEmail"]);
-            if (user != null)
+            var admin = await userManager.FindByEmailAsync(Configuration["AdminUserEmail"]);
+            if (admin != null)
             {
-                await userManager.AddToRoleAsync(user, "admin");
+                await userManager.AddToRoleAsync(admin, "admin");
             }
-        }
-
-        //папка Areas, PartialLayout и подключить к Layout
+            var manager = await userManager.FindByEmailAsync(Configuration["ManagerUserEmail"]);
+            if (manager != null)
+            {
+                await userManager.AddToRoleAsync(manager, "manager");
+            }
+        }              
     }
 }
